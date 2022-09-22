41a70b20316ca37aa5898287460ef554a44190dc
mihneabuz <mihneabuzatu88@gmail.com>
2022-09-23 00:05:56 +0300

    renamed dir and added test script with concurent clients

---
+++ client/clients.sh
@@ -0,0 +1,16 @@
+./target/debug/client -m "fortune" -r 10 -d 1000 &
+./target/debug/client -m "fortune" -r 30 -d 300 &
+
+./target/debug/client -m "increment" -r 50 -d 100 &
+./target/debug/client -m "increment" -r 20 -d 500 &
+./target/debug/client -m "increment" -r 100 -d 50 &
+
+./target/debug/client -m "upload test" -r 10 -d 1000 &
+./target/debug/client -m "upload hello" -r 100 -d 100 &
+./target/debug/client -m "upload world" -r 100 -d 100 &
+
+./target/debug/client -m "download test" -r 10 -d 1000 &
+./target/debug/client -m "download hello" -r 100 -d 100 &
+./target/debug/client -m "download world" -r 100 -d 100 &
+
+./target/debug/client -m "counter" -r 10 -d 1000
